e18b6cc649813f95dfe6d33bc87b77c8e0973812
Brayan-Javier-Gomez <brayanjaviergomez.g@gmail.com>
2021-10-20 20:26:25 -0500

    Login

--- src/app/modules/protected/dashboard/dashboard.component.ts
+++ src/app/modules/protected/dashboard/dashboard.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
 
-  constructor() { }
+  constructor(private router : Router) { }
 
-  ngOnInit(): void {
-  }
+ logout(){
+   console.log("logout")
+   this.router.navigateByUrl('/auth/login')
+ }
 
 }
